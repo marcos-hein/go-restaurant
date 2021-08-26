c61d7543e8b7aefdad86a9a78bf621821aad5075
Marcos Pereira <marcos.hen-ext@via.com.br>
2021-08-26 14:45:05 -0300

    Componentes Input e Modal refatorados

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,23 @@ import {
   useRef,
   useState,
   useCallback,
+  ReactNode,
 } from 'react';
 
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import React from 'react';
+import { IconBaseProps } from 'react-icons/lib';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  placeholder?: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+function Input({ name, icon:Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
